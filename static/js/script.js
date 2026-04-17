c5e84dbc3e8730fedf7998333a62349b706ae1e8
Rounak Prasad <rounak16112006@gmail.com>
2026-04-17 17:19:35 +0530

    Add like to article functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,13 @@
 function validate(event){
-    v = document.getElementById("email").value;
+    v = document.getElementById("feedback").value;
     if(v.indexOf('x') == -1 ){
-        console.log("Error")
         event.preventDefault();
-        alert("Enter a valid email")
+        alert("Feedback can't be empty")
         return false
     }
     return true
 }       
 
+function like(){
+    alert("LIked")
+}
